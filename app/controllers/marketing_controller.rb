f426dfe6c337cf31d1bd4e8e4e0d6e4118acdaad
Odell Purifoy <opurifoy@springhealth.com>
2024-11-03 17:30:13 -0500

    build: Added pricing page

--- app/controllers/marketing_controller.rb
+++ app/controllers/marketing_controller.rb
@@ -1,7 +1,7 @@
 class MarketingController < ApplicationController
   layout 'marketing'
 
-  def root
-    # Render the app/views/marketing/root.html.erb template
-  end
+  def root; end
+
+  def pricing; end
 end
